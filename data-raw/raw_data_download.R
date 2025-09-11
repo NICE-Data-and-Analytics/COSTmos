f241e81dd556f75756ba8b2b5e63992391c2bc71
vtwkam1 <vkwacko007@gmail.com>
2025-09-11 17:22:36 +0100

    Fix Drug Tariff download year-month naming

--- data-raw/raw_data_download.R
+++ data-raw/raw_data_download.R
@@ -23,12 +23,12 @@ url_viii <- "https://www.nhsbsa.nhs.uk/pharmacies-gp-practices-and-appliance-con
 
 # Extract href attribute values
 # Read HTML of webpage into R
-viii_href <- url_viii %>%
-  rvest::read_html() %>%
+viii_href <- url_viii |>
+  rvest::read_html() |>
   # Get all <div> elements with the class "expander-content" for all the sections (Cat A, Cat M, VIIIA, VIIIB and VIIID)
-  rvest::html_elements("div.expander-content") %>%
+  rvest::html_elements("div.expander-content") |>
   # Get the first <a> element in each of the <div> above with the class "ckfile file" - URL for latest data CSV
-  rvest::html_element("a.ckfile.file") %>%
+  rvest::html_element("a.ckfile.file") |>
   # Get value for href attribute
   rvest::html_attr("href")
 
@@ -39,14 +39,14 @@ viii_links <- list(cat_m = stringr::str_subset(viii_href, "Cat%20M"),
                    viii_d = stringr::str_subset(viii_href, "VIIID"))
 
 # Extract dates from file names
-viii_dates <- list(cat_m = list(month = stringr::str_extract(viii_links$cat_m, "\\w{3}(?=%\\d{4}\\.xls)"),
-                                year = stringr::str_extract(viii_links$cat_m, "\\d{4}(?=\\.xls)")),
+viii_dates <- list(cat_m = list(month = stringr::str_extract(viii_links$cat_m, "[:alpha:]+(?=%\\d+\\.xls)"),
+                                year = paste0("20", stringr::str_extract(viii_links$cat_m, "\\d{2}(?=\\.xls)"))),
                    viii_a = list(month = stringr::str_extract(viii_links$viii_a, "(?<=VIIIA%20)[:alpha:]+"),
-                                year = stringr::str_extract(viii_links$viii_a, "\\d{4}(?=\\.xls)")),
+                                year = paste0("20", stringr::str_extract(viii_links$viii_a, "\\d{2}(?=\\.xls)"))),
                    viii_b = list(month = stringr::str_extract(viii_links$viii_b, "(?<=VIIIB%20)[:alpha:]+"),
-                                year = stringr::str_extract(viii_links$viii_b, "\\d{4}(?=\\.xls)")),
+                                year = paste0("20", stringr::str_extract(viii_links$viii_b, "\\d{2}(?=\\.xls)"))),
                    viii_d = list(month = stringr::str_extract(viii_links$viii_d, "(?<=VIIID%20)[:alpha:]+"),
-                                year = stringr::str_extract(viii_links$viii_d, "\\d{4}(?=\\.xls)"))
+                                year = paste0("20", stringr::str_extract(viii_links$viii_d, "\\d{2}(?=\\.xls)")))
                    )
 
 # Function to download file, using the list names
@@ -69,8 +69,11 @@ download_csv <- function(name) {
   # Generate full link for the file that needs downloading
   full_link <- paste0("https://www.nhsbsa.nhs.uk", viii_links[[name]])
   
+  # Detect year and month of file
+  file_ym <- stringr::str_remove_all(lubridate::my(paste(stringr::str_sub(viii_dates[[name]]$month, 1, 3), viii_dates[[name]]$year)), "-|(01)")
+  
   # Generate file name
-  filename <- paste0("drug_tariff_", name, "_", stringr::str_remove_all(lubridate::ym(paste0(viii_dates[[name]]$year, "-", viii_dates[[name]]$month)), "-|(01)"), ".csv")
+  filename <- paste0("drug_tariff_", name, "_", file_ym, ".csv")
   
   # Generate download file path, save to temporary directory
   download_path <- rprojroot::find_package_root_file("inst", "extdata", filename)
@@ -82,7 +85,7 @@ download_csv <- function(name) {
   df <- read_csv(download_path,
                  skip = 5,
                  col_types = viii_read[[name]]$col_types,
-                 col_names = viii_read[[name]]$col_names) %>% 
+                 col_names = viii_read[[name]]$col_names) |> 
     select(all_of(viii_read[[name]]$col_order))
   # Add error handling
   
@@ -97,6 +100,9 @@ download_csv <- function(name) {
   
   # Also save as .rda object
   do.call("use_data", list(as.name(df_name), overwrite = TRUE))
+  
+  # Remove previous versions of this drug tariff section
+  file.remove(stringr::str_subset(list.files(rprojroot::find_package_root_file("inst", "extdata"), pattern = name, full.names = T), file_ym, negate = T))
 }
 
 # Loop through and download all files
@@ -107,29 +113,28 @@ purrr::walk(names(viii_links), download_csv)
 url_ix <- "https://www.nhsbsa.nhs.uk/pharmacies-gp-practices-and-appliance-contractors/drug-tariff/drug-tariff-part-ix"
 
 # Read HTML of webpage into R
-ix_link <- url_ix %>%
-  rvest::read_html() %>%
+ix_link <- url_ix |>
+  rvest::read_html() |>
   # Get first <div> element with the class "expander-content" for the section on the latest year
-  rvest::html_element("div.expander-content") %>%
+  rvest::html_element("div.expander-content") |>
   # Get table
-  rvest::html_element("table table") %>%
+  rvest::html_element("table table") |>
   # Get <a> element in the second cell of the first row
-  rvest::html_element("tr:first-of-type > td:nth-of-type(2) > a") %>%
+  rvest::html_element("tr:first-of-type > td:nth-of-type(2) > a") |>
   # Get value for href attribute
   rvest::html_attr("href")
 
+# Generate ix file year month
+ix_file_ym <- stringr::str_remove_all(
+  lubridate::my(
+    paste(stringr::str_sub(stringr::str_extract(ix_link, "(?<=IX%20)[:alpha:]+"), 1, 3), 
+          stringr::str_extract(ix_link, "\\d{4}(?=\\.csv)")
+          )
+    ), 
+  "-|(01)")
+
 # Generate ix file name
-ix_filename <- paste0("drug_tariff_ix_",
-                      stringr::str_remove_all(
-                        lubridate::ym(
-                          paste0(stringr::str_extract(ix_link, "\\d{4}(?=\\.csv)"),
-                                 "-",
-                                 stringr::str_extract(ix_link, "(?<=IX%20)[:alpha:]+")
-                          )
-                        ),
-                        "-|(01)"),
-                      ".csv"
-)
+ix_filename <- paste0("drug_tariff_ix_", ix_file_ym, ".csv")
 
 # Generate download file path, save to temporary directory
 ix_download_path <- rprojroot::find_package_root_file("inst", "extdata", ix_filename)
@@ -147,7 +152,7 @@ drug_tariff_ix <- read_csv(ix_download_path,
                              "colour", "size_or_weight", "quantity", "quantity_unit_of_measure",
                              "product_order_number", "pack_order_number", "price", "add_dispensing_indicator",
                              "product_snomed_code", "pack_snomed_code", "gtin",
-                             "supplier_snomed_code","bnf_code")) %>% 
+                             "supplier_snomed_code","bnf_code")) |> 
   select(all_of(c("drug_tariff_part", "vmp_name", "amp_name", "supplier_name",
                 "quantity", "quantity_unit_of_measure", "price", "colour", "size_or_weight", 
                 "product_order_number", "pack_order_number", "add_dispensing_indicator",
@@ -161,13 +166,16 @@ write_csv(drug_tariff_ix, ix_download_path)
 # Also save as .rda object
 usethis::use_data(drug_tariff_ix, overwrite = T)
 
+# Remove previous versions of drug tariff IX section
+file.remove(stringr::str_subset(list.files(rprojroot::find_package_root_file("inst", "extdata"), pattern = "ix", full.names = T), ix_file_ym, negate = T))
+
 #PCA
 url_PCA <- "https://www.nhsbsa.nhs.uk/statistical-collections/prescription-cost-analysis-england"
 
 page <- read_html(url_PCA)
 
-elements <- page %>%
-  html_nodes("a") %>%
+elements <- page |>
+  html_nodes("a") |>
   {
     data.frame(
       text = html_text(., trim = TRUE),
@@ -187,8 +195,8 @@ PCA_year <- gsub("(.{4})", "\\1",PCA_year[[1]])
 
 page <- read_html(latest)
 
-elements <- page %>%
-  html_nodes("a") %>%
+elements <- page |>
+  html_nodes("a") |>
   {
     data.frame(
       text = html_text(., trim = TRUE),
@@ -212,7 +220,7 @@ PCA <- PCA[-(1:3),]
 colnames(PCA) <- PCA[1,]
 PCA <- PCA[-1,]
 
-PCA <- PCA %>%
+PCA <- PCA |>
   select("Generic BNF Presentation Name",
          "BNF Chapter Name",
          "SNOMED Code",
