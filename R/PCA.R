b9bf2cffc598ded9700e1c1f7db7a889292c66b9
vtwkam1 <vkwacko007@gmail.com>
2025-10-02 16:33:15 +0100

    Temp fix for could not find function colDef error for PCA.R

--- R/PCA.R
+++ R/PCA.R
@@ -5,19 +5,19 @@ pca_bnf_chapter_choice <- c("All" = "_ALL_", sort(unique(pca_calendar_year$bnf_c
 
 # PCA reactable column specification
 pca_col_spec <- list(
-  generic_bnf_presentation_name = colDef(name = "Generic BNF presentation name"),
-  bnf_chapter_name = colDef(name = "BNF chapter"),
-  snomed_code = colDef(name = "SNOMED code"),
-  unit_of_measure = colDef(name = "Unit of measure"),
-  total_items = colDef(name = "Total items",
-                       format = colFormat(separators = T)),
-  total_quantity = colDef(name = "Total quantity",
-                          format = colFormat(separators = T)),
-  total_cost = colDef(name = "Total cost (£)",
+  generic_bnf_presentation_name = reactable::colDef(name = "Generic BNF presentation name"),
+  bnf_chapter_name = reactable::colDef(name = "BNF chapter"),
+  snomed_code = reactable::colDef(name = "SNOMED code"),
+  unit_of_measure = reactable::colDef(name = "Unit of measure"),
+  total_items = reactable::colDef(name = "Total items",
+                       format = reactable::colFormat(separators = T)),
+  total_quantity = reactable::colDef(name = "Total quantity",
+                          format = reactable::colFormat(separators = T)),
+  total_cost = reactable::colDef(name = "Total cost (£)",
                       cell = function(value) {
                         format(round(value, 2), nsmall = 2, big.mark = ",")
                       }),
-  cost_per_quantity = colDef(name = "Cost per quantity (£)",
+  cost_per_quantity = reactable::colDef(name = "Cost per quantity (£)",
                              cell = function(value) {
                                format(round(value, 2), nsmall = 2, big.mark = ",")
                              })
